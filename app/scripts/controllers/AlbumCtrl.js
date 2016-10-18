2f78247e6a19eaf367379643b5de5acd8e078b3a
RobotMermaid <ondinerangel@gmail.com>
2016-10-18 14:33:28 -0700

    album controller

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,26 @@
  (function() {
      function AlbumCtrl() {
-     	//my guess: make a constructor funciton to build the album vieaw and the song rows
-     }
+     	albumData = {
+		    title: 'The Color',
+		    artist: 'Pablo Picasso',
+		    label: 'Cubism',
+		    year: '1881',
+		    albumArtUrl: 'assets/images/album_covers/01.png',
+		    songs: [
+		         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+		         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+		         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+		         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+		         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+		    ]
+		};
+     };
  
      angular
          .module('blocJams')
          .controller('AlbumCtrl', AlbumCtrl);
  })();
+
+
+
+
