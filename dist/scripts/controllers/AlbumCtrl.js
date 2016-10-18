78125dc9e373e0eb55707c18c90fe4756a9afedd
RobotMermaid <ondinerangel@gmail.com>
2016-10-18 12:02:24 -0700

    collection view controller

---
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,9 @@
+ (function() {
+     function AlbumCtrl() {
+     	//my guess: make a constructor funciton to build the album vieaw and the song rows
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
